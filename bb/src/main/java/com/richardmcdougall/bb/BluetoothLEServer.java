98290061b9a85403d80817e3993fee685336ba12
Jos Boumans <jos@dwim.org>
2019-02-22 10:24:47 -0800

    * update the UUID to match testing

--- bb/src/main/java/com/richardmcdougall/bb/BluetoothLEServer.java
+++ bb/src/main/java/com/richardmcdougall/bb/BluetoothLEServer.java
@@ -62,12 +62,14 @@ public class BluetoothLEServer {
 
 
     public final static UUID kBurnerBoardUUID =
-            UUID.fromString("58fdc6ee-15d1-11e8-b642-0ed5f89f718b");
+            //UUID.fromString("58fdc6ee-15d1-11e8-b642-0ed5f89f718b");
+            UUID.fromString("6e400001-b5a3-f393-e0a9-e50e24dcca9e");
 
     public static final UUID UART_SERVICE_UUID = UUID.fromString("6e400001-b5a3-f393-e0a9-e50e24dcca9e");
     public static final UUID TX_CHAR_UUID = UUID.fromString("6e400002-b5a3-f393-e0a9-e50e24dcca9e");
     public static final UUID RX_CHAR_UUID = UUID.fromString("6e400003-b5a3-f393-e0a9-e50e24dcca9e");
     public static final UUID CCCD = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb");
+
 
     private BluetoothGattCharacteristic mRxCharacteristic;
     private BluetoothGattCharacteristic mTxCharacteristic;
